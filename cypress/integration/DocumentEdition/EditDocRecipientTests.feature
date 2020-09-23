84e2a7aec46ac8b6dc0d0cf628de797e73dcb252
Mohammed-ali <monarc1937@gmail.com>
2020-09-23 14:20:03 +0200

    Enhance tests structure

--- cypress/integration/DocumentEdition/EditDocRecipientTests.feature
+++ cypress/integration/DocumentEdition/EditDocRecipientTests.feature
@@ -1,11 +1,10 @@
 Feature: Edit document recipient
 
-    Scenario: Every submitted document appears in the list
-    Given A user is successfully authenticated
-    When He submits a document 
-    Then The document is anlysed by AI
-
-    Scenario: Edit the recipient for a document
-    Given A document ready to send in the main list 
-    When The document recipient is edited
-    Then Document information is updated
+    Scenario: Add new recipient to document after analysis
+        Given An authenticated user uploads a document
+        And The document status changes to A vérifier or Prêt à l'envoi
+        When I open precision vue of the document
+        And I search for "William ECLANCHER" as a new recipient
+        And I find it as "Dr William ECLANCHER" with speciality "Spécialiste en Médecine Générale" in "Paris"
+        Then I add it as a recipient
+        And It is added succesfully to the recipients list of the doc
--- cypress/integration/DocumentEdition/EditDocRecipientTests/EditDocRecipientTests.js
+++ cypress/integration/DocumentEdition/EditDocRecipientTests/EditDocRecipientTests.js
@@ -1,41 +1,53 @@
-import { Given, When, Then } from "cypress-cucumber-preprocessor/steps";
+import { Given, When, Then, And } from "cypress-cucumber-preprocessor/steps";
 
-Given('A user is successfully authenticated', () => {
+before(() => {
     cy.viewport('macbook-13')
     cy.visit('/')
-    cy.signIn(Cypress.env('EMAIL'), Cypress.env('PASSWORD'))
-    cy.url().should('contain', 'app.lifen.fr')
 })
 
-When('He submits a document', () => {
+Given('An authenticated user uploads a document', () => {
+    cy.signIn(Cypress.env('EMAIL'), Cypress.env('PASSWORD'))
+    cy.url().should('contain', 'app.lifen.fr')
     cy.contains('Boîte d\'envoi').click()
     cy.contains('Nouvel envoi').children('input').attachFile({ filePath: 'CR.pdf', encoding: 'base64' })
     cy.contains('1 document a bien été envoyé vers Lifen', { timeout: 10000 }).should('be.visible')
     cy.contains('Finaliser l\'envoi').click()
 })
 
-Then('The document is anlysed by AI', () => {
+And('The document status changes to A vérifier or Prêt à l\'envoi', () => {
     cy.contains('En cours d\'analyse').should('be.visible')
-    cy.wait(4000)
-})
-
-
-Given('A document ready to send in the main list', () => {
-    cy.contains('Prêt à l\'envoi').first().click()
-})
-
-When('The document recipient is edited', () => {
-    cy.contains('Ajouter un destinataire').click()
-    cy.get('.rbt-input-main').type('Eclancher William').should('have.value', 'Eclancher William')
-    cy.wait(2000)
-    cy.get('#rbt-menu-item-1').then($doctor => {
-        cy.wrap($doctor).invoke('attr', 'aria-label').should('eq', 'William Eclancher')
-        cy.wrap($doctor).click()
+    cy.get('tbody>tr').eq(0).children('td').eq(1).invoke('text').should('satisfy', (status) => {
+        return status == 'Prêt à l\'envoi' || status == 'À vérifier' ? true : false
     })
 })
 
-Then('Document information is updated', () => {
+When('I open precision vue of the document', () => {
+    cy.contains('Prêt à l\'envoi').first().click()
+})
+
+And('I search for {string} as a new recipient', recipientName => {
+    cy.contains('Ajouter un destinataire').click()
+    cy.get('.rbt-input-main').type(recipientName).should('have.value', recipientName)
+})
+
+And('I find it as {string} with speciality {string} in {string}', (fullName, activity, practiceCity) => {
+    cy.wait(2000)
+    cy.get('#rbt-menu-item-1 > a > div > div:nth-child(1)').invoke('text').then( displayedName => {
+        expect(displayedName.toLowerCase()).eq(fullName.toLowerCase())
+    })
+    cy.get('#rbt-menu-item-1 > a > div > span').invoke('text').then(displayedActivity => {
+        expect(displayedActivity.toLowerCase()).eq(activity.toLowerCase())
+    })
+    cy.get('#rbt-menu-item-1 > a > div > div.text-capitalize').invoke('text').then(displayedPracticeCity => {
+        expect(displayedPracticeCity.toLowerCase()).contain(practiceCity.toLowerCase())
+    })
+})
+
+Then('I add it as a recipient', () => {
+    cy.get('#rbt-menu-item-1').click()
+})
+
+And('It is added succesfully to the recipients list of the doc', () => {
     cy.contains('DESTINATAIRES (2)').should('be.visible')
     cy.contains('ECLANCHER').should('exist')
-
 })
